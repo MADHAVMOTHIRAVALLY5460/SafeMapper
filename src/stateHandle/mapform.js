c923976da28ecbf81c5e69e1569abe3a8b4bce63
Madhav M <46835550+MADHAVMOTHIRAVALLY5460@users.noreply.github.com>
2026-02-24 11:01:49 +0530

    Update mapform.js

--- src/stateHandle/mapform.js
+++ src/stateHandle/mapform.js
@@ -2,6 +2,14 @@ import { reactive } from "vue";
 
 export const mapformHandle = reactive({
     coords: [],
+    async getPath () {
+        const APIBase = import.meta.env.VITE_API_BASE
+        const proxyURL = await fetch (`${APIBase}/find-safest-route`) 
+    
+        const data = await proxyURL.json()
+        this.coords.push(data)
+    },
+    
     async postLocation (params) {
         const APIBase = import.meta.env.VITE_API_BASE
         const payload = new URLSearchParams(params).toString()
@@ -9,14 +17,9 @@ export const mapformHandle = reactive({
         const proxyURL = await fetch (`${APIBase}/get-cords?${payload}`, {
             method: "POST",
             headers: {"Content-Type": "application/json"},
-        }).then(() => console.log("Posted!"))
+        }).then(() => {
+            console.log("Posted!")
+            await this.getPath()
+        })
     },
-
-    async getPath () {
-        const APIBase = import.meta.env.VITE_API_BASE
-        const proxyURL = await fetch (`${APIBase}/find-safest-route`) 
-    
-        const data = await proxyURL.json()
-        this.coords.push(data)
-    }
 })
