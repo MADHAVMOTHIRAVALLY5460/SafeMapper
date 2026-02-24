72d952641920471184c64f04d42c2ede6e43cb3f
Madhav M <46835550+MADHAVMOTHIRAVALLY5460@users.noreply.github.com>
2026-02-24 11:40:22 +0530

    Update FormMenu.vue

--- src/components/FormMenu.vue
+++ src/components/FormMenu.vue
@@ -27,7 +27,7 @@
 
                 try {
                     const response = await fetch(`https://safemapperbackend.onrender.com/risk`)
-                    console.log(response.body.text())
+                    console.log(response.body)
                     
                     const riskDisp = document.getElementById("riskfactor")
                     if (riskScore.value > 50 && riskScore < 70) {
