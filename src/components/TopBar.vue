2bdd78ecad645398b61f01ec1c6531b3d854571b
Madhav M <46835550+MADHAVMOTHIRAVALLY5460@users.noreply.github.com>
2026-02-24 12:19:34 +0530

    Update TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -5,7 +5,7 @@
     <div class="w-full h-25 border-[#DEDEDE] border-b-1 flex items-center pl-17">
         <span class="flex items-center">
             <img :src="globe">
-            <h1 class="font-main text-3xl font-semibold text-[#6A6A6A] ml-3">SafeMapper</h1>
+            <h1 class="font-main text-3xl font-semibold text-[#6A6A6A] ml-3">SafeStride</h1>
         </span>
     </div>
 </template>
